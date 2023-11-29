49fa832af653610dfa3a03f09ac8d128b7103321
João Pedro Rosada Volponi <joaopedrovolponi@Joaos-MacBook-Air-2.local>
2023-11-28 23:44:44 -0300

    feat: 🎸 Imagem de Background

--- Tempo/ViewController.swift
+++ Tempo/ViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ // Image Background
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setHierarchy()
+        setContraints()
+        
     }
+    
+    private func setHierarchy() {
+        view.addSubview(backgroundView)
+    }
+    
+    private func setContraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+  
 }
 
