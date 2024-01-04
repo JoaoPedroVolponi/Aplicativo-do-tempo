382d64f5971cd36a4964c5c06bc08768eb636c2c
João Pedro Volponi <jpvrosada@hotmail.com>
2024-01-04 17:17:29 -0300

    refactor: 💡 DailyForecastTableViewCell

--- Tempo/DailyForecastTableViewCell.swift
+++ Tempo/DailyForecastTableViewCell.swift
@@ -8,54 +8,52 @@
 import UIKit
 
 class DailyForecastTableViewCell: UITableViewCell {
+    
     static let identifier: String = "DailyForecast"
     
-    // WeekDayLabel
     private lazy var weekDayLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "TER"
-        label.textColor = UIColor.whiteColor
+        label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
     }()
     
-    // minTemperatureLabel
     private lazy var minTemperatureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "min 25°C"
-        label.textColor = UIColor.whiteColor
+        label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
     }()
     
-    // maxTemperatureLabel
     private lazy var maxTemperatureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "max 30°C"
-        label.textColor = UIColor.whiteColor
+        label.textColor = UIColor.contrastColor
         label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
     }()
     
-    // IconImage
     private lazy var iconImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "cloudIcon")
         return imageView
     }()
     
-    // StackViewHorizontal
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [weekDayLabel, iconImageView, minTemperatureLabel, maxTemperatureLabel])
+        let stackView = UIStackView(arrangedSubviews: [weekDayLabel,
+                                                       iconImageView,
+                                                       minTemperatureLabel,
+                                                       maxTemperatureLabel])
         stackView.axis = .horizontal
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16,
+                                                                     leading: 16,
+                                                                     bottom: 16,
+                                                                     trailing: 16)
         stackView.spacing = 15
         return stackView
     }()
@@ -69,9 +67,17 @@ class DailyForecastTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func loadData(weekDay: String?, min: String?, max: String?, icon: UIImage?) {
+        weekDayLabel.text = weekDay
+        minTemperatureLabel.text = "min \(min ?? "")"
+        maxTemperatureLabel.text = "max \(max ?? "")"
+        iconImageView.image = icon
+    }
+    
     private func setupView() {
         backgroundColor = .clear
         selectionStyle = .none
+        
         setHierarchy()
         setConstraints()
     }
@@ -82,17 +88,9 @@ class DailyForecastTableViewCell: UITableViewCell {
         NSLayoutConstraint.activate([
             weekDayLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 50)
         ])
-        
     }
     
     private func setConstraints() {
-        // StackView
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -15),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-        ])
+        stackView.setConstraintsToParent(contentView)
     }
-    
 }
